66373cde62ec5603d9d0e95685874fba2595583c
yomna <yomihamody@gmail.com>
2022-11-08 19:36:17 +0000

    .DS_STORE

---
+++ task2.1/counter_tb.cpp
@@ -0,0 +1,78 @@
+//Mandatory header files
+//Note the name Vcounter.h for the module counter
+#include "Vcounter.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "vbuddy.cpp"
+
+int main(int argc, char **argv, char **env) {
+    int i;      //i counts the number of clock cycles to simulate
+    int clk;    //clk is the module clock signal
+    
+    Verilated::commandArgs(argc, argv);
+    //init top verilog instance
+    //Instantiate the counter module as Vcounter, which is the name of all generated files. This is the DUT
+    Vcounter* top = new Vcounter;
+    //init trace dump
+    //Turn on signal tracing, and tell verilator to dump the waveform data to counter.vcd
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    top->trace (tfp,99);
+    tfp->open ("counter.vcd");
+    
+    //init Vbuddy
+    //Open and initialise Vbuddy connection. Port path is in vbuddy.cfg
+    if (vbdOpen()!=1) return(-1);
+    vbdHeader("Lab 1: Counter");
+
+    //  initialize simulation inputs
+    //Set initial signal levels. Top is the name of the top-level entity. Only the top-level signals are visible
+    top->clk = 1;
+    top->rst = 1;
+    top->en = 0;
+
+    //  run simulation for many clock cycles
+    //This is the for-loop where simulation happens. i counts the clock cycles
+    for (i=0; i<1000; i++)  {
+
+        //  dump variables into VCD file and toggle clock
+        //This is the for-loop that toggles the clock. It also outputs the trace for each half of the
+        //clock cycles, and forces the model to evaluate on both edges of the clock
+        for (clk=0; clk<2; clk++)  {
+            tfp->dump (2*i+clk);
+            top->clk = !top->clk;
+            top->eval ();
+        }
+
+        // ++++ Send count value to Vbuddy
+        //Output count values to 7-seg display every cycle
+        //vbdHex(4, (int(top->count) >> 16) & 0xF);
+        //vbdHex(3, (int(top->count) >> 8) & 0xF);
+        //vbdHex(2, (int(top->count) >> 4) & 0xF);
+        //vbdHex(1, int(top->count) & 0xF);
+        //vbdCycle(i+1);
+
+        //Alternatively, plot on TFT
+        vbdPlot(int(top->count), 0, 255);
+
+        // ---- end of Vbuddy output section
+        
+    
+        //Change rst and en signals during simulation
+        //ORIGINAL
+        top->rst = (i<1) | (i==15);
+        //top->en = (i>4);
+
+        //TY1
+        //top->en = (i<10) | (i>=12);
+
+        //Vbuddy flag feature
+        top->en = vbdFlag();
+
+        if (Verilated::gotFinish()) exit(0);
+    }
+
+    vbdClose();         // ++++
+    tfp->close();
+    exit(0);
+}
---
+++ task4/counter.sv
@@ -0,0 +1,18 @@
+
+
+module counter #(
+    parameter WIDTH = 8
+)(
+    // interface signals
+    input logic clk, //clock
+    input logic rst, //reset
+    input logic en, //counter enable
+    // create another input here to take in reset value
+    output logic [WIDTH-1:0] count //count output
+);
+
+always_ff @ (posedge clk)
+    if (rst) count <= {WIDTH{1'b0}};
+    else count <= count + {{WIDTH-1{1'b0}},en};
+
+endmodule
